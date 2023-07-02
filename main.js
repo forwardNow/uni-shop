5759d5e7da1710210b409aaac93d25431e87ac89
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-02 13:22:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
 
-// #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import http from './commons/http/http'
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
+uni.$http = http;
+
+
+
+
 const app = new Vue({
     ...App
 })
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-import App from './App.vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif

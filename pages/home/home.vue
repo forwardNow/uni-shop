74dd87332a3bcc8f7a21d9f73ec1c3db7ec19663
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-02 13:53:57 +0800

    Update home.vue

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,7 +1,7 @@
 <template>
   <view>
     <swiper class="swiper" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
-      <swiper-item v-for="(item) in swiperList">
+      <swiper-item v-for="(item, index) in swiperList" :key="index">
         <view class="swiper-item">
           <image :src="item.image_src"></image>
         </view>

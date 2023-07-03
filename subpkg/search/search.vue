4a870fbc4776dcadb7b30c79627789d21a636c16
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-03 17:19:41 +0800

    Update search.vue

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -1,6 +1,14 @@
 <template>
-  <view>
-    search
+  <view class="search-box">
+    <uni-search-bar 
+      radius="16" 
+      focus
+      placeholder="请输入关键词" 
+      clearButton="auto" 
+      cancelButton="none" 
+      @input="handleInput" 
+      bgColor="#fff"
+    />
   </view>
 </template>
 
@@ -8,15 +16,27 @@
   export default {
     data() {
       return {
-        
+        keyword: '',
       }
     },
     methods: {
-      
+      handleInput(value) {
+        clearTimeout(this.timer);
+        
+        this.timer = setTimeout(() => {
+          // console.log(value);
+          this.keyword = value;
+        }, 500)
+      }
     }
   }
 </script>
 
-<style>
-
+<style lang="scss">
+.search-box {
+  z-index: 999;
+  position: sticky;
+  top: 0;
+  background-color: #c00000;
+}
 </style>

44b0cb854c0ef458c758238542411029bfae817f
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-05 11:42:49 +0800

    Update goods_detail.vue

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -5,6 +5,19 @@
         <image class="swiper-image" :src="item.pics_big"></image>
       </swiper-item>
     </swiper>
+    
+    <view class="goods-info">
+      <view class="price">￥{{ goodsDetail.goods_price }}</view>
+      <view class="goods-info-body">
+        <view class="goods-name">{{ goodsDetail.goods_name }}</view>
+        <view class="favi">
+          <uni-icons type="star" size="16"></uni-icons>
+          <text>收藏</text>
+        </view>
+      </view>
+      <view class="express">快递：免运费</view>
+    </view>
+    
   </view>
 </template>
 
@@ -57,6 +70,40 @@
     display: block;
     width: 100%;
     height: 100%;
+  }
+}
+
+.goods-info {
+  padding: 10px;
+  padding-right: 0;
+  
+  .price {
+    margin: 10px 0;
+    font-size: 18px;
+    color: #c00000;
+  }
+  .goods-info-body {
+    display: flex;
+    justify-content: space-between;
+  }
+  .goods-name {
+    font-size: 14px;
+  }
+  .favi {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
+    width: 120px;
+    border-left: solid 1px #efefef;
+    color: #666;
+    font-size: 12px;
+  }
+  .express {
+    margin-top: 12px;
+    font-size: 12px;
+    color: #666;
   }
 }
 </style>

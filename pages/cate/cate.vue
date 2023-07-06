38aede604601912b1eb84ac81011088f946fe2e3
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-06 12:34:13 +0800

    Update cate.vue

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -39,7 +39,10 @@
 </template>
 
 <script>
+  import tabbarBadge from '@/mixins/tabbar-badge';
+  
   export default {
+    mixins: [tabbarBadge],
     onLoad() {
       this.getWindowHeight();
       this.getCateList();

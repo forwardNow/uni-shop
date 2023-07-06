7152431b307ffbccb7aae91c6f042eefbee809cf
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-06 13:16:12 +0800

    Update cart.vue

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,25 +1,47 @@
 <template>
   <view>
-    Cart
+    <view class="cart-title">
+      <uni-icons type="shop" size="18"></uni-icons>
+      <text class="cart-title-text">购物车</text>
+    </view>
+    
+    <template v-for="(goods, i) in cart" :key="i">
+      <my-goods :goods="goods" :radio="true"></my-goods>
+    </template>
   </view>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
   import tabbarBadge from '@/mixins/tabbar-badge';
-  
+
   export default {
     mixins: [tabbarBadge],
     data() {
       return {
-        
+
       }
     },
+    computed: {
+      ...mapState('cart', ['cart']),
+    },
     methods: {
-      
+
     }
   }
 </script>
 
-<style>
+<style lang="scss">
+  .cart-title {
+    height: 40px;
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    padding-left: 5px;
+    border-bottom: 1px solid #efefef;
 
+    .cart-title-text {
+      margin-left: 10px;
+    }
+  }
 </style>

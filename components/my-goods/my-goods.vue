efe5b09385fc2755a36e0a8f9e7a73d6b1fc5265
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-06 13:08:59 +0800

    Create my-goods.vue

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,77 @@
+<template>
+  <view class="goods-item" @click="handleClickGoodsItem(goods)">
+
+    <view class="goods-item-left">
+      <image class="goods-img" :src="goods.goods_small_logo"></image>
+    </view>
+
+    <view class="goods-item-right">
+
+      <view class="goods-name">{{ goods.goods_name }}</view>
+
+      <view class="goods-info-box">
+        <text class="goods-price">{{ goods.goods_price ? `￥${goods.goods_price}` : '' }}</text>
+      </view>
+
+    </view>
+
+  </view>
+</template>
+
+<script>
+  export default {
+    name: "my-goods",
+    props: {
+      goods: {
+        type: Object,
+        default: {},
+      },
+    },
+    data() {
+      return {
+
+      };
+    },
+    methods: {
+      handleClickGoodsItem(goods) {
+        this.$emit('click', goods);
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .goods-item {
+    display: flex;
+    padding: 12px 10px;
+    border-bottom: solid 1px #efefef;
+  }
+
+  .goods-item-left {
+    margin-right: 4px;
+  }
+
+  .goods-img {
+    display: block;
+    width: 100px;
+    height: 100px;
+    background-color: #efefef;
+  }
+
+  .goods-item-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .goods-name {
+    font-size: 14px;
+  }
+
+  .goods-info-box {}
+
+  .goods-price {
+    font-size: 16px;
+    color: #c00000;
+  }
+</style>

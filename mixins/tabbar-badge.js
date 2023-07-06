e25e8f2be71440ff4b2a5795dc9290adcd136050
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-06 12:34:09 +0800

    Create tabbar-badge.js

---
+++ mixins/tabbar-badge.js
@@ -0,0 +1,20 @@
+import { mapGetters } from 'vuex';
+
+export default {
+  onShow() {
+    this.setBadge();
+  },
+  
+  computed: {
+    ...mapGetters('cart', ['total']),
+  },
+  
+  methods: {
+    setBadge() {
+      uni.setTabBarBadge({
+        index: 2, // 索引
+        text: String(this.total) // 注意：text 的值必须是字符串，不能是数字
+      });
+    }
+  }
+}

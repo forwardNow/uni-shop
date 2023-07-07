5a4a60fae365e35419fd770b39c1cc89eb3d32de
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-07 13:42:30 +0800

    Update store.js

--- store/store.js
+++ store/store.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import cart from './cart';
+import user from './user';
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
     cart,
+    user,
   },
 })
 

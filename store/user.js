ffae32538912c66453a6930ada40827331e7e21f
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-07 13:42:31 +0800

    Create user.js

---
+++ store/user.js
@@ -0,0 +1,45 @@
+const ADDRESS_KEY = 'address';
+
+function getAddress() {
+  const addressStr = uni.getStorageSync(ADDRESS_KEY) || '{}';
+  
+  const address = JSON.parse(addressStr);
+  
+  return address;
+}
+
+export default {
+  namespaced: true,
+  
+  state: {
+    address: getAddress(),
+  },
+  
+  getters: {
+    hasAddress(state) {
+      return state.address && state.address.provinceName;
+    },
+    
+    fullAddr(state, getters) {
+      if (!getters.hasAddress) {
+        return '';
+      }
+      
+      const { provinceName, cityName, countyName, detailInfo } = state.address;
+      
+      return provinceName + cityName + countyName + detailInfo;
+    },
+  },
+  
+  mutations: {
+    updateAddress(state, address) {
+      state.address = address;
+      
+      this.commit('user/saveAddressToStorage');
+    },
+    
+    saveAddressToStorage(state) {
+      uni.setStorageSync(ADDRESS_KEY, JSON.stringify(state.address));
+    },
+  }
+}

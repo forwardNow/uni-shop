44f9f269b910888d595b0f6e9b938c0e392846cd
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-08 11:37:54 +0800

    Create my-settle.vue

---
+++ components/my-settle/my-settle.vue
@@ -0,0 +1,71 @@
+<template>
+  <view class="settle-container">
+    
+    <label class="radio-box">
+      <radio :checked="true" color="#C00000" ></radio>
+      <text>全选</text>
+    </label>
+    
+    <view class="amount-box">
+      合计：<text class="amount">￥123.45</text>
+    </view>
+    
+    <view class="settle-btn">结算(0)</view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"my-settle",
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+.settle-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
+  z-index: 999;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  
+  padding-left: 10px;
+  width: 100%;
+  height: 50px;
+  
+  background-color: #fff;
+  
+  font-size: 14px;
+  
+  .radio-box {
+    display: flex;
+    align-items: center;
+    width: 90px;
+  }
+  .amount-box {
+    flex: 1;
+  }
+  .amount {
+    font-weight: bold;
+    color: #c00000;
+    font-size: 16px;
+  }
+  .settle-btn {
+    padding: 0 10px;
+    min-width: 100px;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    color: #fff;
+    background-color: #c00000;
+    font-size: 16px;
+  }
+}
+</style>

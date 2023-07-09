c8d6d72eb8d41a615e4054fb9afff2fde8e3fae5
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-09 19:18:11 +0800

    Update my-userinfo.vue

--- components/my-userinfo/my-userinfo.vue
+++ components/my-userinfo/my-userinfo.vue
@@ -1,20 +1,56 @@
 <template>
-  <view>
-    
+  <view class="my-userinfo-container">
+    <view class="top-box">
+      <image class="avatar" :src="userInfo.avatarUrl"></image>
+      <view class="nickname">{{ userInfo.nickName }}</view>
+    </view>
   </view>
 </template>
 
 <script>
+  import { mapState } from 'vuex';
+  
   export default {
-    name:"my-userinfo",
+    name: "my-userinfo",
     data() {
       return {
-        
+
       };
-    }
+    },
+    computed: {
+      ...mapState('user', ['userInfo']),
+    },
   }
 </script>
 
 <style lang="scss">
-
+.my-userinfo-container {
+  height: 100%;
+  background-color: #f4f4f4;
+  
+  .top-box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    
+    height: 400rpx;
+    
+    background-color: #c00000;
+  }
+  .avatar {
+    display: block;
+    width: 90px;
+    height: 90px;
+    border-radius: 45px;
+    border: 2px solid white;
+    box-shadow: 0 1px 5px #000;
+  }
+  .nickname {
+    margin-top: 10px;
+    color: #fff;
+    font-weight: bold;
+    font-size: 16px;
+  }
+}
 </style>

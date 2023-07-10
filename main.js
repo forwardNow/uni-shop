8b4eda90c111461756e54c52bfdea0c1af15c92d
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-10 11:36:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 
 import Vue from 'vue'
 import App from './App'
-import http from './commons/http/http'
+import { createHttp } from './commons/http/http'
 import store from './store/store.js'
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
-uni.$http = http;
+uni.$http = createHttp(store);
 
 uni.$showToast = (title = '', duration = 3000, icon = 'none') => {
   uni.showToast({ title, duration, icon });

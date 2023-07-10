b834e314d47e720d90e2ebe5831349d364af01c6
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-10 11:20:21 +0800

    Update my-login.vue

--- components/my-login/my-login.vue
+++ components/my-login/my-login.vue
@@ -1,114 +1,146 @@
 <template>
-  <view class="login-container" >
-    
+  <view class="login-container">
+
     <uni-icons type="contact-filled" size="100" color="#AFAFAF">></uni-icons>
-    
-    <button 
-      type="primary" 
-      class="btn-login" 
-      @click="handleLoginBtn"
-    >
+
+    <button type="primary" class="btn-login" @click="handleLoginBtn">
       一键登录
     </button>
-    
+
     <view class="tips-text">登录后尽享更多权益</view>
-    
+
   </view>
 </template>
 
 <script>
-  import { mapMutations } from 'vuex';
-  
+  import {
+    mapMutations,
+    mapState
+  } from 'vuex';
+
   export default {
-    name:"my-login",
+    name: "my-login",
     data() {
       return {
-        
+
       };
     },
+
+    computed: {
+      ...mapState('user', ['redirectInfo']),
+    },
+
     methods: {
-      ...mapMutations('user', ['updateUserInfo', 'updateToken']),
-      
+      ...mapMutations('user', ['updateUserInfo', 'updateToken', 'updateRedirectInfo']),
+
       async handleLoginBtn() {
-        const [error, res] = await uni.getUserProfile({ desc: '用于完善会员资料' }).catch((e) => e);
-        
+        const [error, res] = await uni.getUserProfile({
+          desc: '用于完善会员资料'
+        }).catch((e) => e);
+
         if (error && error.errMsg === 'getUserProfile:fail auth deny') {
           uni.$showToast('您取消了登录授权！');
           return;
         }
-        
-        const { userInfo } = res;
-        
+
+        const {
+          userInfo
+        } = res;
+
         this.updateUserInfo(userInfo);
-        
+
         this.login(res);
       },
-      
+
       async login(info) {
         const [error, result] = await uni.login().catch((e) => e);
-        
+
         if (error || result.errMsg !== 'login:ok') {
           uni.$showToast('登录失败！');
           return;
         }
-        
-        const data =  {
+
+        const data = {
           code: result.code,
           encryptedData: info.encryptedData,
           iv: info.iv,
           rawData: info.rawData,
           signature: info.signature
         };
-        
+
         // 此 API 登录失败
         const res = await uni.$http.post('/api/public/v1/users/wxlogin', data);
-        
+
         this.updateToken('test---token');
+
+        this.navBack();
       },
+
+      navBack() {
+        if (!this.redirectInfo) {
+          return;
+        }
+
+        const {
+          openType,
+          from
+        } = this.redirectInfo;
+
+        if (openType === 'switchTab') {
+          uni.switchTab({
+            url: from,
+            success: () => {
+              this.updateRedirectInfo(null);
+            }
+          });
+        }
+      },
+
     },
   }
 </script>
 
 <style lang="scss">
-.login-container {
-  position: relative;
-  
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  
-  height: 750rpx;
-  
-  background-color: #f8f8f8;
-  
-  overflow: hidden;
-  
-  &::after {
-    content: '';
-    
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    
-    width: 100%;
-    height: 40px;
-    border-radius: 100%;
-    background-color: #fff;
-    
-    transform: translateY(50%);
+  .login-container {
+    position: relative;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    height: 750rpx;
+
+    background-color: #f8f8f8;
+
+    overflow: hidden;
+
+    &::after {
+      content: '';
+
+      position: absolute;
+      bottom: 0;
+      left: 0;
+
+      width: 100%;
+      height: 40px;
+      border-radius: 100%;
+      background-color: #fff;
+
+      transform: translateY(50%);
+    }
+
+
+    .btn-login {
+      width: 90%;
+      border-radius: 100px;
+      margin: 15px 0;
+      background-color: #c00000;
+    }
+
+    .tips-text {
+      font-size: 12px;
+      color: #666;
+    }
   }
-  
-  
-  .btn-login {
-    width: 90%;
-    border-radius: 100px;
-    margin: 15px 0;
-    background-color: #c00000;
-  }
-  .tips-text {
-    font-size: 12px;
-    color: #666;
-  }
-}
 </style>

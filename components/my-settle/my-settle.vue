a57f9f5c30fcee05e6e7f8d3855edd461b4dfef9
吴钦飞 <wuqinfei_csdn@163.com>
2023-07-10 10:55:37 +0800

    Update my-settle.vue

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -1,109 +1,142 @@
 <template>
   <view class="settle-container">
-    
+
     <label class="radio-box" @click="handleClickRadio">
       <radio :checked="isSelectAll" color="#C00000"></radio>
       <text>全选</text>
     </label>
-    
+
     <view class="amount-box">
       合计：<text class="amount">￥{{ checkedGoodsAmount }}</text>
     </view>
-    
+
     <view class="settle-btn" @click="handleClickSettleBtn">结算({{ checkedCount }})</view>
   </view>
 </template>
 
 <script>
-  import { mapGetters, mapMutations } from 'vuex';
-  
+  import {
+    mapGetters,
+    mapMutations
+  } from 'vuex';
+
   export default {
-    name:"my-settle",
+    name: "my-settle",
     data() {
       return {
-        
+
       };
     },
     computed: {
       ...mapGetters('cart', ['total', 'checkedCount', 'checkedGoodsAmount']),
       ...mapGetters('user', ['hasAddress', 'isLogin']),
-      
+
       isSelectAll() {
         return this.total === this.checkedCount;
       },
-      
+
       isCheckedGoods() {
         return this.checkedCount > 0;
       },
     },
-    
+
     methods: {
       ...mapMutations('cart', ['updateAllGoodsState']),
-      
+
       handleClickRadio() {
         this.updateAllGoodsState(!this.isSelectAll);
       },
-      
+
       handleClickSettleBtn() {
         if (!this.isCheckedGoods) {
           uni.$showToast('请选择要结算的商品！');
           return;
         }
-        
+
         if (!this.hasAddress) {
           uni.$showToast('请选择收货地址！');
           return;
         }
-        
+
         if (!this.isLogin) {
-          uni.$showToast('请先登录！');
+          this.delayNavToLogin(3);
         }
       },
+
+      delayNavToLogin(delaySecondes) {
+        this.showTips(delaySecondes);
+
+        this.timer = setInterval(() => {
+          delaySecondes -= 1;
+
+          if (delaySecondes <= 0) {
+            clearInterval(this.timer);
+            uni.switchTab({
+              url: '/pages/my/my'
+            })
+            return;
+          }
+
+          this.showTips(delaySecondes)
+        }, 1000)
+      },
+
+      showTips(num) {
+        uni.showToast({
+          icon: 'none',
+          title: `请先登录, ${num}秒后跳转登录`,
+          mask: true,
+          duration: 1500
+        });
+      }
     },
   }
 </script>
 
 <style lang="scss">
-.settle-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  
-  z-index: 999;
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  
-  padding-left: 10px;
-  width: 100%;
-  height: 50px;
-  
-  background-color: #fff;
-  
-  font-size: 14px;
-  
-  .radio-box {
+  .settle-container {
     display: flex;
     align-items: center;
-    width: 90px;
-  }
-  .amount-box {
-    flex: 1;
-  }
-  .amount {
-    font-weight: bold;
-    color: #c00000;
-    font-size: 16px;
-  }
-  .settle-btn {
-    padding: 0 10px;
-    min-width: 100px;
+    justify-content: space-between;
+
+    z-index: 999;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+
+    padding-left: 10px;
+    width: 100%;
     height: 50px;
-    line-height: 50px;
-    text-align: center;
-    color: #fff;
-    background-color: #c00000;
-    font-size: 16px;
+
+    background-color: #fff;
+
+    font-size: 14px;
+
+    .radio-box {
+      display: flex;
+      align-items: center;
+      width: 90px;
+    }
+
+    .amount-box {
+      flex: 1;
+    }
+
+    .amount {
+      font-weight: bold;
+      color: #c00000;
+      font-size: 16px;
+    }
+
+    .settle-btn {
+      padding: 0 10px;
+      min-width: 100px;
+      height: 50px;
+      line-height: 50px;
+      text-align: center;
+      color: #fff;
+      background-color: #c00000;
+      font-size: 16px;
+    }
   }
-}
 </style>
